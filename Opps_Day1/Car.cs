1bb0de4d1569ab003e6722f216608587fbb8f6e9
Praveen <praveen211096@gmail.com>
2024-03-03 12:43:40 +0530

    Oops Concepts

---
+++ Opps_Day1/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opps_Day1
+{
+    public class Car
+    {
+        public int _sum;
+
+        public Car(int sum)
+        {
+            _sum = sum;
+        }
+
+
+        public void display()
+        {
+            Console.WriteLine(_sum);
+        }
+
+    }
+}
---
+++ Opps_Day1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opps_Day1
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            //Class name object name = new ConstructorName();
+            Car obj = new Car(20); //Compiler will create default 
+
+            
+            //obj.sum = 20;
+            obj.display();
+            //Console.WriteLine(obj.sum);
+
+            Car obj2 = new Car(30); //Compiler will create default 
+            obj2.display();
+            Console.ReadKey();
+        }
+    }
+}
