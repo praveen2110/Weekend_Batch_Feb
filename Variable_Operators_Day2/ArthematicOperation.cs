9438051c0c1ff24f1f25f73e0a1ca642fc73fcb6
Praveen <praveen211096@gmail.com>
2024-03-03 11:34:23 +0530

    Solution of Previous class

---
+++ Variable_Operators_Day2/ArthematicOperation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_Operators_Day2
+{
+    public class ArthematicOperation
+    {
+        public double mydivision(double num1, double num2 = 2)
+        {
+            double division = num1 / num2;
+
+            return division;
+        }
+        public double myModulues(double a, double b)
+        {
+            double mod = a % b;
+
+            return mod;
+        }
+
+        public bool isPrime(int n,int divisor = 2)
+        {
+
+            if (n == 2)
+            {
+                return true;
+            }
+            else if (n % divisor != 0 && (divisor * divisor > n || isPrime(n, divisor + 1)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            //Recursion 
+
+            //return n == 2 || (n % divisor != 0 && (divisor * divisor > n || isPrime(n, divisor + 1)));
+            
+        }
+        
+
+    }
+}
---
+++ Variable_Operators_Day2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_Operators_Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.WriteLine("Enter the value of a");
+            a = Convert.ToInt32(Console.ReadLine()); //2
+
+            //int result = a++; // result = a+1 //Uniary Post Increamantal
+
+            //int result = a--; // result = a -1 // uniary Post decremental 
+
+            //int result = ++a; // result = a +1 //Uniary Pre Incremental 
+            int result = --a; // result = a -1 // uniary Pre decremental 
+
+            Console.WriteLine(result);
+            Console.WriteLine(a);
+
+            bool noolresult = (a == 8) ? (a > 2 ) ? true : false : false;
+
+            
+
+            
+
+
+
+                //Console.WriteLine(result);
+
+
+                Console.ReadKey();
+
+            //Operators
+
+            //Write a program to check if the number is prime or not 
+            // 
+
+            //Break and continue 
+
+            // Loops 
+        }
+
+       
+    }
+}
