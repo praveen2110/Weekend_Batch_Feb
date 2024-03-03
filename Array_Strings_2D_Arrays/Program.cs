b3593060a4a050ad44909bf0e2a0b19f401b1ac9
Praveen <praveen211096@gmail.com>
2024-03-03 11:44:42 +0530

    Array_Demo_Code changes

--- Array_Strings_2D_Arrays/Program.cs
+++ Array_Strings_2D_Arrays/Program.cs
@@ -10,6 +10,61 @@ namespace Array_Strings_2D_Arrays
     {
         static void Main(string[] args)
         {
+            //Arrays and Strings 
+            //< datatype >[] < variable name > = new int[size of array];
+            int[] a = new int[10];
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 1; i <= 20; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        a[j] = i;
+                        j++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Enter the rows and column of Matericse:");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            int column = Convert.ToInt32(Console.ReadLine());
+
+            int[,] A = new int[rows,column];
+
+            int n = 1;
+            
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < column;j++)
+                {
+                    while (j < column)
+                    {
+                        if (n % 2 == 0)
+                        {
+                            A[i, j] = n;
+                            j++;
+                        }
+                        n++;
+                    }                    
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < column;j++)
+                {
+                    Console.Write($"{ A[i, j] }");
+                    Console.Write(" ");
+                }
+                Console.WriteLine("");
+            }
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
